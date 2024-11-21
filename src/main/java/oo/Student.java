e4f6b6b98b7d2ec888644d3bd7c7db6e89584295
CHENJO20 <CHENJO20@oocl.com>
2024-11-21 14:04:49 +0800

    feat, update student and teacher class

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,26 @@
 package oo;
 
-public class Student {
+public class Student extends Person {
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object anotherStudent) {
+        if (this == anotherStudent) return true;
+        if (!(anotherStudent instanceof Student student)) return false;
+        return id == student.id;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a student.", name, age);
+    }
+
 }
