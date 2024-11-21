0df02ff9b0a35393d8a53a9a9edbda1fceddddf1
CHENJO20 <CHENJO20@oocl.com>
2024-11-21 14:20:40 +0800

    feat, update Klass and Student

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -2,6 +2,9 @@ package oo;
 
 public class Student extends Person {
 
+    private static final String STUDENT_ROLE = "student";
+    private Klass klass;
+
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
@@ -20,7 +23,14 @@ public class Student extends Person {
 
     @Override
     public String introduce() {
-        return super.introduce() + " I am a student.";
+        return super.introduce() + " I am a " + STUDENT_ROLE + ". " + "I am in class " + klass.getNumber();
     }
 
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return this.klass == klass;
+    }
 }
