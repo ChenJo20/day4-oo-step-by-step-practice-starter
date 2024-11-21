6e2f9f68b31c5e5c7b5652b44d17c95183065b46
CHENJO20 <CHENJO20@oocl.com>
2024-11-21 14:22:59 +0800

    refactor, make teacher role constant

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -2,13 +2,15 @@ package oo;
 
 public class Teacher extends Person {
 
+    private static final String TEACHER_ROLE = "teacher";
+
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
     @Override
     public String introduce() {
-        return super.introduce() + " I am a teacher.";
+        return super.introduce() + " I am a " + TEACHER_ROLE + ".";
     }
 
     @Override
