f2fae3296af5bec533bd4528e7659a628c1d07e7
CHENJO20 <CHENJO20@oocl.com>
2024-11-21 13:39:29 +0800

    feat, update Person class & add introduce method

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,19 @@
 package oo;
 
 public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public void introduce() {
+        System.out.printf("My name is %s. I am %d years old.", name, age);
+        My name is Tom. I am 18 years old.
+    }
 }
